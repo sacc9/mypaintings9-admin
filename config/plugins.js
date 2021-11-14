bc54db9ad1085dc59744d2e8ebf04b6ef63c52d9
unknown <matteoalberta@live.de>
2021-11-14 12:07:22 +0100

    make it better

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dwj3obdhh',
+        api_key: '794476965987723',
+        api_secret: '1zPxd_ah1agtO9Lzqa38yFPLyco',
+      },
+    },
+    // ...
+  });
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a99dad75b31452564c243d80018d948e'),
+    },
+  },
+});
